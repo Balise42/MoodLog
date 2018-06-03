fd8793834adcd67b84454a9c90c7731cedd57b0a
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2018-06-03 20:55:45 +0200

    handling toggle buttons in details

--- app/src/main/java/fr/pasithee/moodlog/activities/DetailLogFragment.kt
+++ app/src/main/java/fr/pasithee/moodlog/activities/DetailLogFragment.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ToggleButton
 import fr.pasithee.moodlog.R
 import kotlinx.android.synthetic.main.fragment_detail_log.*
 
@@ -23,9 +24,19 @@ class DetailLogFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         logActivityButton.setOnClickListener {
             val details = ArrayList<String>()
-            details.add("Happy")
-            details.add("Plop")
+            goThroughView(tableLayout, details)
             (activity as LogActivity).logDetails(details)
+        }
+    }
+
+    private fun goThroughView(view: ViewGroup, details: ArrayList<String>) {
+        for (i in 0 until view.childCount) {
+            val elem = view.getChildAt(i)
+            if (elem is ToggleButton && elem.isChecked) {
+                details.add(elem.textOn.toString())
+            } else if (elem is ViewGroup) {
+                goThroughView(elem, details)
+            }
         }
     }
 
